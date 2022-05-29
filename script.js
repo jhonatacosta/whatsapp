f8260a5ec7c2c639921795b12d0e0411828e734c
Jhonata Costa <74380029+jhonatacosta@users.noreply.github.com>
2022-05-29 17:10:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const API_WHATSAPP = "https://web.whatsapp.com/send?phone=";
+
+const cliente = document.querySelector("#number-client");
+function send() {
+  let phone = cliente.value;
+  let url = API_WHATSAPP + phone;
+  window.open(url);
+}
+
+(function () {
+  $("form").each(function () {
+    $(this)
+      .find("input")
+      .keypress(function (e) {
+        // Enter pressed?
+        if (e.which == 10 || e.which == 13) {
+          this.form.submit();
+        }
+      });
+
+    $(this).find("input[type=submit]").hide();
+  });
+});
